d1db6829cf9463118bd8ef63a0b4ae55b7b93431
KIMURA Yusuke <s1320077@u-aizu.ac.jp>
2025-06-27 10:51:53 +0900

    Won/lost message added

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import random
 
+
 def main():
     print("Rolling dice...")
     die1 = random.randint(1,6)
     die2 = random.randint(1,6)
+    total = die1 + die2
     print(f"Die 1: {die1}")
     print(f"Die 2: {die2}")
-    print(f"Total value: {die1 + die2}")
+    print(f"Total value: {total}")
+
+    if total > 7:
+        print("You won!")
+    else:
+        print("You lost")
+
 
 if __name__ == "__main__":
     main()
