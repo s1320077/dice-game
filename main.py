3350e19ea8f9c2dd349fc1ee042f05679143a19d
s1320077 <s1320077@u-aizu.ac.jp>
2025-06-27 17:01:51 +0900

    Added functions

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import random
 
 
-def main():
+def handle_input() -> str:
     name = ""
-    print("What is your name?")
     while not name:
         name = input("> ")
+    return name
+
+
+def is_win(total: int) -> bool:
+    return total > 7
+
+
+def main():
+    print("What is your name?")
+    name = handle_input()
     print(f"Hello, {name}")
 
     print("Rolling dice...")
-    die1 = random.randint(1,6)
-    die2 = random.randint(1,6)
+    die1 = random.randint(1, 6)
+    die2 = random.randint(1, 6)
     total = die1 + die2
     print(f"Die 1: {die1}")
     print(f"Die 2: {die2}")
     print(f"Total value: {total}")
 
-    if total > 7:
+    if is_win(total):
         print(f"{name} won!")
     else:
         print(f"{name} lost")
