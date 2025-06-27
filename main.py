7a9263eea5e3e58f9b56e27dbc0921b2fd099ba2
s1320077 <s1320077@u-aizu.ac.jp>
2025-06-27 17:47:28 +0900

    Implemented the game in class

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import random
 
 
+class DiceGame:
+    def __init__(self, name):
+        self.name = name
+        self.total = 0
+
+    def is_win(self) -> bool:
+        return self.total > 7
+
+    def start(self):
+        print("Rolling dice...")
+        die1 = random.randint(1, 6)
+        die2 = random.randint(1, 6)
+        self.total = die1 + die2
+        print(f"Die 1: {die1}")
+        print(f"Die 2: {die2}")
+        print(f"Total value: {self.total}")
+
+        if self.is_win():
+            print(f"{self.name} won!")
+        else:
+            print(f"{self.name} lost")
+
+
 def handle_input() -> str:
     name = ""
     while not name:
@@ -8,27 +31,13 @@ def handle_input() -> str:
     return name
 
 
-def is_win(total: int) -> bool:
-    return total > 7
-
-
 def main():
     print("What is your name?")
     name = handle_input()
     print(f"Hello, {name}")
 
-    print("Rolling dice...")
-    die1 = random.randint(1, 6)
-    die2 = random.randint(1, 6)
-    total = die1 + die2
-    print(f"Die 1: {die1}")
-    print(f"Die 2: {die2}")
-    print(f"Total value: {total}")
-
-    if is_win(total):
-        print(f"{name} won!")
-    else:
-        print(f"{name} lost")
+    game = DiceGame(name)
+    game.start()
 
 
 if __name__ == "__main__":
