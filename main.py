df678d8984b47db8f0af59167f8317d2c6a601c7
s1320077 <s1320077@u-aizu.ac.jp>
2025-07-03 12:37:51 +0900

    add value table, not probability table

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import random
+from functools import reduce
 
 
 class DiceGame:
-    def __init__(self, name):
+    def __init__(self, name, winning_threshold):
         self.name = name
         self.total = 0
+        self.is_winning_condition = lambda x: x > winning_threshold
 
     def is_win(self) -> bool:
-        return self.total > 7
+        return self.is_winning_condition(self.total)
 
     def start(self):
         print("Rolling dice...")
@@ -23,6 +25,21 @@ class DiceGame:
         else:
             print(f"{self.name} lost")
 
+    def print_value_table(self) -> None:
+        print("value table")
+        print("   1  2  3  4  5  6")
+        for i in range(1, 7):
+            print(f"{i} ", end="")
+            for j in range(1, 7):
+                print(f"{i + j:2} ", end="")
+            print("")
+
+    def calc_win_probability(self) -> float:
+        win_count = sum(
+            self.is_winning_condition(i + j) for i in range(1, 7) for j in range(1, 7)
+        )
+        return win_count / 36
+
 
 def handle_input() -> str:
     name = ""
@@ -36,7 +53,9 @@ def main():
     name = handle_input()
     print(f"Hello, {name}")
 
-    game = DiceGame(name)
+    game = DiceGame(name, 7)
+    game.print_value_table()
+    print(f"yout win probability: {game.calc_win_probability()}")
     game.start()
 
 
